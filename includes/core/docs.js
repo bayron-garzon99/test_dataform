8986a838276c19654751947e72b33a2a0331bce3
Bayron Garzón <bayron.garzon@99minutos.com>
2024-11-07 21:26:22 +0000

    structure project for lakes/zones/entities schema in dataplex

--- includes/core/docs.js
+++ includes/core/docs.js
@@ -1,21 +1,23 @@
-const columns_events_flow= {
- tracking_id= `columns description`,
- country= `columns description`,
- status_list= `columns description`,
- event_name_list= `columns description`,
- status_cnt= `columns description`,
- status_list_remove_self_loop= `columns description`,
- status_remove_self_loop_cnt= `columns description`,
- has_invalid_transitions= `columns description`,
- invalid_transitions= `columns description`,
- extraction_time= `columns description`,
- shipment_created_at= `columns description`,
- shipments_origin_statio= `columns description`,
- shipments_origin_zone= `columns description`,
- shipments_destination_station= `columns description`,
- shipments_destination_zone= `columns description`,
-}
+const description = "event's flow of state machine";
+const columns_events_flow = {
+    tracking_id : `columns description`,
+    country : `columns description`,
+    status_list : `columns description`,
+    event_name_list : `columns description`,
+    status_cnt : `columns description`,
+    status_list_remove_self_loop : `columns description`,
+    status_remove_self_loop_cnt : `columns description`,
+    has_invalid_transitions : `columns description`,
+    invalid_transitions : `columns description`,
+    extraction_time : `columns description`,
+    shipment_created_at : `columns description`,
+    shipments_origin_statio : `columns description`,
+    shipments_origin_zone : `columns description`,
+    shipments_destination_station : `columns description`,
+    shipments_destination_zone : `columns description`,
+};
 
-module.exports =  {
-  columns_events_flow
+module.exports = {
+    description,
+    columns_events_flow
 };
