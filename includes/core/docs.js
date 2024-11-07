e348ee51d937049208347003fa2f46905d18ec4c
bayron-garzon99 <bayron.garzon@99minutos.com>
2024-11-07 19:35:40 +0000

    add: structure to develop with dataform

---
+++ includes/core/docs.js
@@ -0,0 +1,21 @@
+const columns_events_flow= {
+ tracking_id= `columns description`,
+ country= `columns description`,
+ status_list= `columns description`,
+ event_name_list= `columns description`,
+ status_cnt= `columns description`,
+ status_list_remove_self_loop= `columns description`,
+ status_remove_self_loop_cnt= `columns description`,
+ has_invalid_transitions= `columns description`,
+ invalid_transitions= `columns description`,
+ extraction_time= `columns description`,
+ shipment_created_at= `columns description`,
+ shipments_origin_statio= `columns description`,
+ shipments_origin_zone= `columns description`,
+ shipments_destination_station= `columns description`,
+ shipments_destination_zone= `columns description`,
+}
+
+module.exports =  {
+  columns_events_flow
+};
