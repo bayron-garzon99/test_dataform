761409f5d709abb0d920917d9aa84b6d2955af3d
bayron-garzon99 <bayron.garzon@99minutos.com>
2024-11-07 22:56:31 +0000

    add: the whole scheme to develop analytical engineering

---
+++ includes/example/docs.js
@@ -0,0 +1,11 @@
+const description = "example table";
+const columns = {
+    iteration : `columns description`,
+    country : `columns description`,
+    created : `columns description`
+};
+
+module.exports = {
+    description,
+    columns
+};
