89bf66fbe2d6236cd1b4e04e655c237ccab4eb35
bayron-garzon99 <bayron.garzon@99minutos.com>
2024-11-08 21:52:53 +0000

    add: blacklit_custom_values table

---
+++ includes/analytics/curated/docs.js
@@ -0,0 +1,9 @@
+const description_blacklist_custom = "custom in blacklist";
+const columns_blacklist_custom = {
+    customer_userid_value: `column with id of custom users in blacklist`,
+};
+
+module.exports = {
+    description_blacklist_custom,
+    columns_blacklist_custom
+};
