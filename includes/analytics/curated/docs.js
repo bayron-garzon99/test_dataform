68c5db98ccf1fc42cee55b7dd15313ad394d269e
bayron-garzon99 <bayron.garzon@99minutos.com>
2024-11-11 20:00:57 +0000

    add:blacklist_shipments_trackingId from .parquet in bucket analytical_engineering

--- includes/analytics/curated/docs.js
+++ includes/analytics/curated/docs.js
@@ -1,9 +1,18 @@
+// ---- //
 const description_blacklist_custom = "custom in blacklist";
 const columns_blacklist_custom = {
     customer_userid_value: `column with id of custom users in blacklist`,
 };
+// ---- //
+const description_blacklist_shipments_trackingId = "custom in blacklist";
+const columns_blacklist_shipments_trackingId ={
+    shipments_trackingId:  `column with id of custom users in blacklist`,
+}
+
 
 module.exports = {
     description_blacklist_custom,
-    columns_blacklist_custom
+    columns_blacklist_custom,
+    description_blacklist_shipments_trackingId,
+    columns_blacklist_shipments_trackingId
 };
